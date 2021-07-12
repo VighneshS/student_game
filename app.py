53ef268f5c4d87681fcb18a1ef817ff743b25d35
VighneshS <vighneshsivaraman@gmail.com>
2021-07-11 21:49:41 -0500

    Assignment 6 done ✅

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+import atexit
 import datetime
+import json
 import os
 from dataclasses import dataclass
 
+import redis
 import sqlalchemy
-from flask import Flask, render_template, request, jsonify
+from apscheduler.schedulers.background import BackgroundScheduler
+from flask import Flask, render_template, request, jsonify, Response
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine, ForeignKey, orm
 from sqlalchemy.orm import relationship
@@ -14,6 +18,13 @@ username = os.environ['UD_DB_USERNAME']
 password = os.environ['UD_DB_PASSWORD']
 
 secretKey = os.environ['SECRET_KEY']
+
+redisHost = os.environ['REDIS_HOST']
+redisPassword = os.environ['REDIS_PASSWORD']
+
+GAME_CHANNEL = 'game_student'
+
+r = redis.StrictRedis(host=redisHost, port=6379, password=redisPassword, db=0, charset='utf-8', decode_responses=True)
 
 app = Flask(__name__)
 app.secret_key = secretKey
@@ -70,27 +81,48 @@ class Student(db.Model):
 
 @dataclass
 class Game(db.Model):
-    __tablename__ = 'games'
+    __tablename__ = 'game'
     id: int
-    teacher_id: str
-    student_id: str
+    teacher_id: int
+    student_id: int
     admin_name: str
-    question: str
-    answer: str
     created_on: datetime
-    updated_on: datetime
-    score: int
 
     id = db.Column(db.Integer(), primary_key=True, nullable=False)
     teacher_id = db.Column(sqlalchemy.Integer(), ForeignKey('teacher.id'))
     teacher = relationship("Teacher", back_populates="game", lazy='joined')
     student_id = db.Column(sqlalchemy.Integer(), ForeignKey('student.id'))
     student = relationship("Student", back_populates="game", lazy='joined')
+    questions = relationship("QuestionAnswer", backref="game", lazy='joined')
     admin_name = db.Column(sqlalchemy.VARCHAR(200))
+    created_on = db.Column(sqlalchemy.TIMESTAMP)
+
+    @property
+    def serializable(self):
+        """Return object data in easily serializable format"""
+        return {
+            'id': self.id,
+            'teacher': {} if not self.teacher else self.teacher.serializable,
+            'student': {} if not self.student else self.student.serializable,
+            'questions': self.questions,
+            'admin_name': self.admin_name,
+            'created_on': self.created_on.isoformat() + 'Z'
+        }
+
+
+@dataclass
+class QuestionAnswer(db.Model):
+    __tablename__ = 'question_answers'
+    id: int
+    game_id: int
+    question: str
+    answer: str
+    score: int
+
+    id = db.Column(db.Integer(), primary_key=True, nullable=False)
+    game_id = db.Column(sqlalchemy.Integer(), ForeignKey('game.id'))
     question = db.Column(sqlalchemy.VARCHAR(200))
     answer = db.Column(sqlalchemy.VARCHAR(200))
-    created_on = db.Column(sqlalchemy.TIMESTAMP)
-    updated_on = db.Column(sqlalchemy.TIMESTAMP)
     score = db.Column(sqlalchemy.Integer())
 
     @property
@@ -98,8 +130,6 @@ class Game(db.Model):
         """Return object data in easily serializable format"""
         return {
             'id': self.id,
-            'teacher': self.teacher.serializable,
-            'student': self.student.serializable,
             'question': self.question,
             'answer': self.answer,
             'score': self.score,
@@ -108,9 +138,8 @@ class Game(db.Model):
 
 @app.route('/', methods=['GET'])
 def home():
-    teachers = Teacher.query.all()
-    print(teachers)
-    if len(teachers) <= 0:
+    students = Student.query.all()
+    if len(students) <= 0:
         return render_template('index.html')
     else:
         return render_template('quiz.html')
@@ -122,7 +151,6 @@ def create_name(data):
     db.session.commit()
     games = Game.query.options(orm.joinedload('*')).all()
     teacher = Teacher.query.all()
-    print(teacher, student)
     if teacher and student and not games:
         game = Game(teacher=teacher[0], student=student)
         db.session.add(game)
@@ -130,14 +158,15 @@ def create_name(data):
     return teacher
 
 
-def update_answer(answer: str):
-    game = Game.query.one()
-    game.answer = answer
+def update_answer(answer: str, identity: int):
+    a = QuestionAnswer.query.filter(QuestionAnswer.id == identity).one()
+    a.answer = answer
     db.session.commit()
-    return game
+    return a
 
 
 def delete_all_data():
+    QuestionAnswer.query.delete()
     Game.query.delete()
     Teacher.query.delete()
     Student.query.delete()
@@ -145,16 +174,49 @@ def delete_all_data():
     return {"status": "Delete success"}
 
 
+def event_stream():
+    pubsub = r.pubsub(ignore_subscribe_messages=True)
+    pubsub.subscribe(GAME_CHANNEL)
+    # TODO: handle client disconnection.
+    for message in pubsub.listen():
+        yield 'data: %s\n\n' % message['data']
+
+
+def publish_message(message):
+    r.publish(GAME_CHANNEL, message)
+
+
+def publish_game_data():
+    data = {}
+    games = Game.query.options(orm.joinedload('*')).all()
+    if games:
+        data = \
+            [dict(g.serializable, student={} if not g.student else g.student.serializable,
+                  teacher={} if not g.teacher else g.teacher.serializable,
+                  questions=[] if not g.questions else [dict(q.serializable) for q in g.questions])
+             for g in games][0]
+    publish_message(json.dumps(data))
+    db.session.commit()
+
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=publish_game_data, trigger="interval", seconds=10)
+scheduler.start()
+
+# Shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
+
+
 @app.route('/name', methods=['POST'])
 def save_name():
-    print(request.get_json()['name'])
     return jsonify(create_name(request.get_json()))
 
 
 @app.route('/answer', methods=['POST'])
 def save_answer():
-    print(request.get_json()['answer'])
-    return jsonify(update_answer(request.get_json()['answer']))
+    answer = request.get_json()['answer']
+    question_id = request.get_json()['id']
+    return jsonify(update_answer(answer=answer, identity=question_id))
 
 
 @app.route('/end', methods=['POST'])
@@ -169,15 +231,7 @@ def hello_world():
 
 @app.route('/stream')
 def game_stream():
-    data = {}
-    games = Game.query.options(orm.joinedload('*')).all()
-    teacher = Teacher.query.all()
-    student = Student.query.all()
-    print(teacher, student, games)
-    if games:
-        data = [dict(g.serializable, student=g.student.serializable, teacher=g.teacher.serializable)
-                for g in games][0]
-    return jsonify(data)
+    return Response(event_stream(), mimetype="text/event-stream")
 
 
 if __name__ == '__main__':
--- static/quiz_scripts.js
+++ static/quiz_scripts.js
@@ -4,15 +4,39 @@ let waiting
 let quiz
 let student_name
 let teacher_name
-let answer
-let last_answer
-let last_score
-let question
+let gameId
+let time_elapsed
 
-function saveAnswer() {
+const question_id = '{{question_id}}'
+const student_answer = '{{student_answer}}'
+const score_sent = '{{score_sent}}'
+const last_question = '{{last_question}}'
+const quiz_div = `
+<label for="question{{question_id}}"><strong>Question {{question_id}}: </strong>
+        <span id="question{{question_id}}">{{last_question}}</span></label><br>
+    <label for="answer{{question_id}}"><strong>Answer: </strong></label>
+    <input id="answer{{question_id}}" type="text">
+    <input id="save_name{{question_id}}" class="btn btn-primary" value="Submit" onclick="saveAnswer({{question_id}}, '#answer{{question_id}}')" name="name" type="button"><br>
+    <label for="last_answer{{question_id}}"><strong>Last saved Answer: </strong><span id="last_answer{{question_id}}">{{student_answer}}</span></label><br>
+    <label for="last_score{{question_id}}"><strong>Score from Teacher: </strong><span id="last_score{{question_id}}">{{score_sent}}</span></label><br><br><br>
+`
+
+function createQuestionsBlock(questionAnswers) {
+    quiz.empty()
+    for (const questionAnswersKey in questionAnswers) {
+        quiz.append(quiz_div.replaceAll(question_id, questionAnswers[questionAnswersKey]['id'])
+            .replaceAll(student_answer, questionAnswers[questionAnswersKey]['answer'])
+            .replaceAll(score_sent, questionAnswers[questionAnswersKey]['score'])
+            .replaceAll(last_question, questionAnswers[questionAnswersKey]['question']))
+    }
+
+}
+
+function saveAnswer(id, answer_div) {
     spinner.show()
     let payLoad = {
-        'answer': answer.val()
+        'answer': $(answer_div).val(),
+        'id': id
     }
     $.ajax({
         type: 'POST',
@@ -21,7 +45,6 @@ function saveAnswer() {
         dataType: 'json',
         data: JSON.stringify(payLoad)
     }).done(function (data) {
-        console.log(data);
         spinner.hide()
     });
 }
@@ -34,7 +57,6 @@ function endQuiz() {
         contentType: "application/json",
         dataType: 'json'
     }).done(function (data) {
-        console.log(data);
         spinner.hide()
         window.location.replace('/')
     });
@@ -46,35 +68,55 @@ $(function () {
     quiz = $('#quiz')
     student_name = $('#student_name')
     teacher_name = $('#teacher_name')
-    answer = $('#answer')
-    last_answer = $('#last_answer')
-    last_score = $('#last_score')
-    question = $('#question')
+    time_elapsed = $('#time_elapsed')
     waiting.show()
     spinner.hide()
     quiz.hide()
     spinner.hide()
-    let dataInterval = setInterval(function () {
-        $.ajax({
-            type: 'GET',
-            url: "/stream",
-            contentType: "application/json",
-            dataType: 'json'
-        }).done(function (data) {
-            console.log(data);
-            if (data['student'] && data['teacher'] && data['question']) {
+
+
+    function sse() {
+        let source = new EventSource('/stream');
+        source.onmessage = function (e) {
+            let data = JSON.parse(e.data)
+            gameId = data.id
+            if (data['student'] && data['teacher'] && data['questions']) {
                 student_name.html(data['student']['name'])
                 teacher_name.html(data['teacher']['name'])
-                question.html(data['question'])
-                last_answer.html(data['answer'])
-                last_score.html(data['score'])
+                let questions = data['questions']
+                createQuestionsBlock(questions)
+                let countDownDate = new Date(data['created_on']).getTime()
+                let x = setInterval(function () {
+
+                    // Get today's date and time
+                    let now = new Date().getTime();
+
+                    // Find the distance between now and the count down date
+                    let distance = now - countDownDate;
+
+                    // Time calculations for days, hours, minutes and seconds
+                    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+                    // Display the result in the element with id="demo"
+                    time_elapsed.html(days + "d " + hours + "h "
+                        + minutes + "m " + seconds + "s ")
+                }, 1000);
                 quiz.show()
+                spinner.hide()
                 waiting.hide()
             }
+            console.log(data);
+
             if (!data['student']) {
-                clearInterval(dataInterval)
+                source.close()
                 window.location.replace('/')
             }
-        });
-    }, 10000);
+
+        };
+    }
+
+    sse();
 })
